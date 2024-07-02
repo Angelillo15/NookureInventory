d8cf97d3104ef74ff83343803c28171774750205
Angelillo15 <contact@angelillo15.es>
2024-07-02 13:31:21 +0200

    Change default impl

--- NookureInventory-Test/src/main/java/com/nookure/core/inv/I18nMyPluginAdapter.java
+++ NookureInventory-Test/src/main/java/com/nookure/core/inv/I18nMyPluginAdapter.java
@@ -2,15 +2,16 @@ package com.nookure.core.inv;
 
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.minimessage.MiniMessage;
+import org.bukkit.entity.Player;
 
-public class I18nMyPluginAdapter extends I18nAdapter {
+public class I18nMyPluginAdapter extends I18nAdapter<Player> {
   @Override
-  public String translate(String key, boolean isKey) {
+  public String translate(String key, boolean isKey, Player player) {
     return key;
   }
 
   @Override
-  public Component translateComponent(String key, boolean isKey) {
+  public Component translateComponent(String key, boolean isKey, Player player) {
     return MiniMessage.miniMessage().deserialize(key);
   }
 }
