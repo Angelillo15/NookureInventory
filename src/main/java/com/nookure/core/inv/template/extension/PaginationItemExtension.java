31fe168ae26bc1233ac64520cec916b6beebef81
Angelillo15 <contact@angelillo15.es>
2024-07-02 23:00:11 +0200

    Change exception type

--- src/main/java/com/nookure/core/inv/template/extension/PaginationItemExtension.java
+++ src/main/java/com/nookure/core/inv/template/extension/PaginationItemExtension.java
@@ -1,5 +1,6 @@
 package com.nookure.core.inv.template.extension;
 
+import com.nookure.core.inv.exception.UserFriendlyRuntimeException;
 import io.pebbletemplates.pebble.extension.AbstractExtension;
 import io.pebbletemplates.pebble.extension.Function;
 import io.pebbletemplates.pebble.template.EvaluationContext;
@@ -14,21 +15,22 @@ public class PaginationItemExtension extends AbstractExtension implements Functi
     return Map.of("pagination", this);
   }
 
-  public record PaginationData(long start, long end, int lastPage) {}
+  public record PaginationData(long start, long end, int lastPage) {
+  }
 
   @Override
-  public Object execute(Map<String, Object> args, PebbleTemplate self, EvaluationContext context, int lineNumber) {;
+  public Object execute(Map<String, Object> args, PebbleTemplate self, EvaluationContext context, int lineNumber) {
     Long rows = (Long) args.get("rows");
     Long columns = (Long) args.get("columns");
     Integer page = (Integer) args.get("page");
     Integer total = (Integer) args.get("total");
 
     if (rows == null || columns == null || page == null || total == null) {
-      throw new IllegalArgumentException("Missing required arguments");
+      throw new UserFriendlyRuntimeException("Missing required arguments for pagination ---> pagination(rows, columns, page, total)");
     }
 
     if (rows < 0 || columns < 0 || page < 0 || total < 0) {
-      throw new IllegalArgumentException("Arguments must be greater than 0");
+      throw new UserFriendlyRuntimeException("Pagination arguments must be positive numbers");
     }
 
     long start = (page - 1) * rows * columns;
