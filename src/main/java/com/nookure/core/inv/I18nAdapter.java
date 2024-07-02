3b1d2e7c65e176ab6f00a67241bd35c802de07c0
Angelillo15 <contact@angelillo15.es>
2024-07-02 13:31:01 +0200

    Add to i18n adapter the ability to pass a Player

--- src/main/java/com/nookure/core/inv/I18nAdapter.java
+++ src/main/java/com/nookure/core/inv/I18nAdapter.java
@@ -1,8 +1,9 @@
 package com.nookure.core.inv;
 
 import net.kyori.adventure.text.Component;
+import org.jetbrains.annotations.Nullable;
 
-public abstract class I18nAdapter {
+public abstract class I18nAdapter<T> {
   /**
    * Translate a key or a text to a string
    *
@@ -10,7 +11,7 @@ public abstract class I18nAdapter {
    * @param isKey If the key is a key or a text
    * @return The translated string
    */
-  public abstract String translate(String key, boolean isKey);
+  public abstract String translate(String key, boolean isKey, @Nullable T player);
 
   /**
    * Translate a key or a text to a component
@@ -19,7 +20,29 @@ public abstract class I18nAdapter {
    * @param isKey If the key is a key or a text
    * @return The translated component
    */
-  public abstract Component translateComponent(String key, boolean isKey);
+  public abstract Component translateComponent(String key, boolean isKey, @Nullable T player);
+
+  /**
+   * Translate a key or a text to a string
+   *
+   * @param key   The key to translate
+   * @param isKey If the key is a key or a text
+   * @return The translated string
+   */
+  public String translate(String key, boolean isKey) {
+    return translate(key, isKey, null);
+  }
+
+  /**
+   * Translate a key or a text to a component
+   *
+   * @param key   The key to translate
+   * @param isKey If the key is a key or a text
+   * @return The translated component
+   */
+  public Component translateComponent(String key, boolean isKey) {
+    return translateComponent(key, isKey, null);
+  }
 
   /**
    * Translate text to a string
