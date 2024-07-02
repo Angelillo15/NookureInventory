52fb4af41b2b8830b84f319df01db13cf44ed230
Angelillo15 <contact@angelillo15.es>
2024-07-02 17:49:29 +0200

    Add plugin repositories

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,12 @@
 rootProject.name = "NookureInventory"
 include("NookureInventory-Test")
 
+pluginManagement {
+  repositories {
+    gradlePluginPortal()
+  }
+}
+
 plugins {
   id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
 }
