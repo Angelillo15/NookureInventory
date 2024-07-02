2d2c934c38fe5d51134e3638063c8c967db6aff1
Angelillo15 <contact@angelillo15.es>
2024-07-02 17:35:17 +0200

    Add a new Generic refering to the type of player

--- src/main/java/com/nookure/core/inv/parser/item/ItemConverter.java
+++ src/main/java/com/nookure/core/inv/parser/item/ItemConverter.java
@@ -2,8 +2,8 @@ package com.nookure.core.inv.parser.item;
 
 import com.nookure.core.inv.I18nAdapter;
 
-public abstract class ItemConverter<T> {
-  public abstract T convert(Item item, I18nAdapter adapter);
+public abstract class ItemConverter<T, P> {
+  public abstract T convert(Item item, I18nAdapter<P> adapter);
 
   public abstract Item convert(T t);
 }
